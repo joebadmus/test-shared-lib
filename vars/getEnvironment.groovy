f2e728ec6e5dc65551470b4e3e29052785758db1
joebadmus <joebadmus@gmail.com>
2023-05-17 16:21:11 +0100

    add function

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -1,3 +1,12 @@
+import groovy.json.JsonSlurper
+
+def extractEnv(){
+    def jsonFile = new File("${env.WORKSPACE}/environment.json")
+    def jsonSlurper = new JsonSlurper()
+    def jsonData = jsonSlurper.parseText(jsonFile.text)
+    return jsonData.env
+}
 def call(){
-    // echo "Hello from shared"
+def env = extractEnv()
+return env
 }
