10dca8565315126d638c350597130a2bcd890f47
joebadmus <joebadmus@gmail.com>
2023-05-18 14:21:44 +0100

    test

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -5,7 +5,7 @@ def call(String settingfile){
         // def jsonFile = new File("$settingfile")
         // def jsonSlurper = new JsonSlurper()
         // def jsonData = jsonSlurper.parseText(jsonFile.text)
-        cat "$settingfile"
+        echo "$settingfile"
         def json = readJSON(file: "$settingfile")
 	    def env = json['results']['env']
 
