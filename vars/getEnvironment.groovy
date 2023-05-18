5268a1cacb22dc0cb95ed5b54f350462f2826fb9
joebadmus <joebadmus@gmail.com>
2023-05-18 14:09:45 +0100

    from to jsonreader

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -2,11 +2,14 @@ import groovy.json.JsonSlurper
 
 def call(String settingfile){
     try{
-        def jsonFile = new File("$settingfile")
-        def jsonSlurper = new JsonSlurper()
-        def jsonData = jsonSlurper.parseText(jsonFile.text)
-        echo "setting file read"
-        return jsonData.env
+        // def jsonFile = new File("$settingfile")
+        // def jsonSlurper = new JsonSlurper()
+        // def jsonData = jsonSlurper.parseText(jsonFile.text)
+        def json = readJSON(file: "$settingfile")
+	    def env = json['results']['env']
+
+        echo "setting file read $env"
+        return json['results']['env']
     }catch(e){
          error "Job failure due invalid JSON file." 
     }
