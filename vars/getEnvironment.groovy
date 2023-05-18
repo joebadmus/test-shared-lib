e909ade636fa9c65d59a7ef9cbf1089a261f3da3
joebadmus <joebadmus@gmail.com>
2023-05-18 06:33:33 +0100

    refactor lib

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -1,12 +1,17 @@
 import groovy.json.JsonSlurper
 
-def extractEnv(){
-    def jsonFile = new File("${env.WORKSPACE}/environment.json")
+// def extractEnv(){
+//     def jsonFile = new File("${env.WORKSPACE}/environment.json")
+//     def jsonSlurper = new JsonSlurper()
+//     def jsonData = jsonSlurper.parseText(jsonFile.text)
+//     return jsonData.env
+// }
+def call(String settingfile){
+// def env = extractEnv()
+// return env
+    // def jsonFile = new File("${env.WORKSPACE}/environment.json")
+    def jsonFile = new File("$settingfile")
     def jsonSlurper = new JsonSlurper()
     def jsonData = jsonSlurper.parseText(jsonFile.text)
     return jsonData.env
-}
-def call(){
-def env = extractEnv()
-return env
 }
