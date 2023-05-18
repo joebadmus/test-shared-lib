0f08e1fdc92c5504be410d728166e6fde2a4932d
joebadmus <joebadmus@gmail.com>
2023-05-18 06:53:43 +0100

    add try catch

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -1,17 +1,13 @@
 import groovy.json.JsonSlurper
 
-// def extractEnv(){
-//     def jsonFile = new File("${env.WORKSPACE}/environment.json")
-//     def jsonSlurper = new JsonSlurper()
-//     def jsonData = jsonSlurper.parseText(jsonFile.text)
-//     return jsonData.env
-// }
 def call(String settingfile){
-// def env = extractEnv()
-// return env
-    // def jsonFile = new File("${env.WORKSPACE}/environment.json")
-    def jsonFile = new File("$settingfile")
-    def jsonSlurper = new JsonSlurper()
-    def jsonData = jsonSlurper.parseText(jsonFile.text)
-    return jsonData.env
+    try{
+        def jsonFile = new File("$settingfile")
+        def jsonSlurper = new JsonSlurper()
+        def jsonData = jsonSlurper.parseText(jsonFile.text)
+        return jsonData.env
+    }catch(e){
+         error "Job failure due invalid JSON file." 
+    }
+
 }
