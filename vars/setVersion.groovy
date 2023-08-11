84547bdac9aa902e42aac3ae10deb04624e257f7
joebadmus <joebadmus@gmail.com>
2023-08-11 15:19:00 +0100

    try fix 2

--- vars/setVersion.groovy
+++ vars/setVersion.groovy
@@ -5,26 +5,26 @@ def call(String branch) {
     sh 'ls -ltr'
     sh 'pwd'
     
-    def currentVersion = readFile file "version.txt"
-    def versionParts = currentVersion.split('\\.')
-    def majorVersion = versionParts[0].toInteger()
-    def minorVersion = versionParts[1].toInteger()
-    def patchVersion = versionParts[2].toInteger()
+    // def currentVersion = readFile file "version.txt"
+    // def versionParts = currentVersion.split('\\.')
+    // def majorVersion = versionParts[0].toInteger()
+    // def minorVersion = versionParts[1].toInteger()
+    // def patchVersion = versionParts[2].toInteger()
 
-    if (branch == 'master') {
-        majorVersion++
-        minorVersion = 0
-        patchVersion = 0
-    }
-    else if (branch == 'release') {
-        minorVersion++
-        patchVersion = 0
-    }
-    else{
-        patchVersion++
-    }
+    // if (branch == 'master') {
+    //     majorVersion++
+    //     minorVersion = 0
+    //     patchVersion = 0
+    // }
+    // else if (branch == 'release') {
+    //     minorVersion++
+    //     patchVersion = 0
+    // }
+    // else{
+    //     patchVersion++
+    // }
 
-    def newVersion = "${majorVersion}.${minorVersion}.${patchVersion}"
-    writeFile file: 'version.txt', text: newVersion
-    echo "New version: ${newVersion}"
+    // def newVersion = "${majorVersion}.${minorVersion}.${patchVersion}"
+    // writeFile file: 'version.txt', text: newVersion
+    // echo "New version: ${newVersion}"
 }
