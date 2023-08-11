ab09610304c32c1aad273a0056c1af2e7aa9e76f
joebadmus <joebadmus@gmail.com>
2023-08-11 11:34:46 +0100

    fix version path

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -2,7 +2,7 @@
 def call(String settingfile) {
     echo "Getting cuurent Version...."
     sh 'ls -la'
-    def currentVersion = readFile file: "../version.txt"
+    def currentVersion = readFile file: "version.txt"
     echo "Current version is ${currentVersion}" 
 
     return currentVersion
