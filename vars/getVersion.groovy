614d583f6b389ab1b2f7413fb526ab48bd683891
joebadmus <joebadmus@gmail.com>
2024-01-24 13:52:26 +0000

    Test2

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -3,7 +3,7 @@ def call() {
     echo "Getting cuurent Version...."
     sh 'ls -la'
     // def currentVersion = readFile file: "version.txt"
-    echo "Current version is NONE" 
+    echo "Current version is ${NEW_ENV}" 
 
     // return currentVersion
 }
